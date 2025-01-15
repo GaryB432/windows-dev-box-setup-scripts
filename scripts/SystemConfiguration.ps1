35601d114178009791a49ad9f67ec5b27060a8d7
Gary Bortosky <garybort@gmail.com>
2025-01-15 13:47:47 -0800

    change dev mode reg key

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -1,4 +1,4 @@
 
 
 #--- Enable developer mode on the system ---
-Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowAllTrustedApps" /d "1"
