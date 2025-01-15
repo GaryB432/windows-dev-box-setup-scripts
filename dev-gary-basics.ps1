12a07f980878f776e957a06c504efebc3f72757b
Gary Bortosky <garybort@gmail.com>
2025-01-15 12:39:21 -0800

    Update dev-gary-basics.ps1

--- dev-gary-basics.ps1
+++ dev-gary-basics.ps1
@@ -37,7 +37,7 @@ executeScript "Browsers.ps1";
 #--- Tools ---
 choco install -y nodejs-lts # Node.js LTS, Recommended for most users
 # choco install -y nodejs # Node.js Current, Latest features
-choco install -y visualstudio2017buildtools
+# choco install -y visualstudio2017buildtools
 # choco install -y visualstudio2017-workload-vctools
 choco install -y python2 # Node.js requires Python 2 to build native modules
 
